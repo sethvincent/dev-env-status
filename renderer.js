fb61ce021dfc6f121d34e3b8d73122aecee5afde
sethvincent <sethvincent@gmail.com>
2016-11-23 10:38:18 -0800

    loading state while waiting on data events

--- renderer.js
+++ renderer.js
@@ -3,7 +3,12 @@ var html = require('yo-yo')
 
 var stream = check()
 
-var state = {}
+var state = {
+  os: { loading: true },
+  node: { loading: true },
+  npm: { loading: true },
+  git: { loading: true }
+}
 
 stream.on('data', function (data) {
   if (data.type === 'os') {
@@ -43,71 +48,89 @@ function header () {
 }
 
 function osView (state) {
-  return html`<div class="">
-    <h2 class="">About your operating system</h2>
-    <p>You are using version ${state.release} of ${state.name}.</p>
-  </div>`
+  if (state.loading) {
+    return html`<div class="">
+      <h2>Checking your operating system ...</h2>
+    </div>`
+  } else {
+    return html`<div class="">
+      <h2 class="">About your operating system</h2>
+      <p>You are using version ${state.release} of ${state.name}.</p>
+    </div>`
+  }
 }
 
 function nodeView (state) {
-  var status = state.exists ? success() : fail()
+  if (state.loading) {
+    return html`<h2 class="">Checking node ...</h2>`
+  } else {
+    var status = state.exists ? success() : fail()
 
-  function fail () {
-    return html`<p>💥❗️ No, node is not yet installed, but we can help with that! ❗️💥 </p>`
-  }
+    function fail () {
+      return html`<p>💥❗️ No, node is not yet installed, but we can help with that! ❗️💥 </p>`
+    }
 
-  function success () {
-    return html`<div>
-      <p>✨💖 Yes! Node is installed! 💖✨</p>
-      <p>Version: ${state.version}</p>
-      <p>Path: ${state.path}</p>
+    function success () {
+      return html`<div>
+        <p>✨💖 Yes! Node is installed! 💖✨</p>
+        <p>Version: ${state.version}</p>
+        <p>Path: ${state.path}</p>
+      </div>`
+    }
+
+    return html`<div class="">
+      <h2 class="">Is node installed?</h2>
+      ${status}
     </div>`
   }
-
-  return html`<div class="">
-    <h2 class="">Is node installed?</h2>
-    ${status}
-  </div>`
 }
 
 function npmView (state) {
-  var status = state.exists ? success() : fail()
+  if (state.loading) {
+    return html`<h2 class="">Checking npm ...</h2>`
+  } else {
+    var status = state.exists ? success() : fail()
 
-  function fail () {
-    return html`<p>💥❗️ No, npm is not yet installed, but we can help with that! ❗️💥 </p>`
-  }
+    function fail () {
+      return html`<p>💥❗️ No, npm is not yet installed, but we can help with that! ❗️💥 </p>`
+    }
 
-  function success () {
-    return html`<div>
-      <p>✨💖 Yes! npm is installed! 💖✨</p>
-      <p>Version: ${state.version}</p>
-      <p>Path: ${state.path}</p>
+    function success () {
+      return html`<div>
+        <p>✨💖 Yes! npm is installed! 💖✨</p>
+        <p>Version: ${state.version}</p>
+        <p>Path: ${state.path}</p>
+      </div>`
+    }
+
+    return html`<div class="">
+      <h2 class="">Is npm installed?</h2>
+      ${status}
     </div>`
   }
-
-  return html`<div class="">
-    <h2 class="">Is npm installed?</h2>
-    ${status}
-  </div>`
 }
 
 function gitView (state) {
-  var status = state.exists ? success() : fail()
+  if (state.loading) {
+    return html`<h2 class="">Checking git ...</h2>`
+  } else {
+    var status = state.exists ? success() : fail()
 
-  function fail () {
-    return html`<p>💥❗️ No, git is not yet installed, but we can help with that! ❗️💥 </p>`
-  }
+    function fail () {
+      return html`<p>💥❗️ No, git is not yet installed, but we can help with that! ❗️💥 </p>`
+    }
 
-  function success () {
-    return html`<div>
-      <p>✨💖 Yes! git is installed! 💖✨</p>
-      <p>Version: ${state.version}</p>
-      <p>Path: ${state.path}</p>
+    function success () {
+      return html`<div>
+        <p>✨💖 Yes! git is installed! 💖✨</p>
+        <p>Version: ${state.version}</p>
+        <p>Path: ${state.path}</p>
+      </div>`
+    }
+
+    return html`<div class="">
+      <h2 class="">Is git installed?</h2>
+      ${status}
     </div>`
   }
-
-  return html`<div class="">
-    <h2 class="">Is git installed?</h2>
-    ${status}
-  </div>`
 }
